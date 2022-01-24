a72e1bfe86f4f42778f79089e12559025ae3ce5c
Jason Siefken <siefkenj@gmail.com>
2022-01-23 21:16:37 -0500

    Fix URL normalization so book can be served from non-root

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -3,12 +3,12 @@
  * `foo.html` and `foo.html#bar` are both normalized to `/foo.html`.
  */
 export function normalizeUrl(url: string): string {
-    return new URL(url, window.location.origin).pathname;
+    return new URL(url, window.location.href).pathname;
 }
 /**
  * Like `normalizeUrl`, but includes the hash
  */
 export function normalizeUrlWithHash(url: string): string {
-    const urlObj = new URL(url, window.location.origin);
+    const urlObj = new URL(url, window.location.href);
     return urlObj.pathname + urlObj.hash;
 }
