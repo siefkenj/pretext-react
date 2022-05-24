3b8430a9d117c431103fb9cde06321fbc36467d7
Jason Siefken <siefkenj@gmail.com>
2022-05-24 18:01:56 -0400

    Cleaned up nav buttons

--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -1,76 +1,43 @@
 import React from "react";
-import { ToolbarItem, useToolbarState } from "reakit";
-import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { navActions, nextPrevParentSelector } from "../features/nav/navSlice";
+import { useAppSelector } from "../app/hooks";
+import { nextPrevParentSelector } from "../features/nav/navSlice";
+import { InternalAnchor } from "./links";
 
 export function PageFooter() {
-    const toolbar = useToolbarState();
     const navTargets = useAppSelector(nextPrevParentSelector);
-    const dispatch = useAppDispatch();
 
     return (
         <div className="ptx-content-footer">
-            <ToolbarItem
-                {...toolbar}
-                as="a"
-                href={navTargets.prev?.id || "#"}
+            <InternalAnchor
+                href={navTargets.prev?.href || "#"}
                 className="previous-button button"
                 title={
                     navTargets.prev
                         ? `Previous (${navTargets.prev.title})`
                         : "Previous"
                 }
-                disabled={!navTargets.prev}
-                onClick={() => {
-                    if (navTargets.prev) {
-                        dispatch(
-                            navActions.setCurrentPage(
-                                navTargets.prev.id || null
-                            )
-                        );
-                    }
-                }}
             >
                 <span className="icon">&lt;</span>
                 <span className="name">Prev</span>
-            </ToolbarItem>
-            <ToolbarItem
-                {...toolbar}
-                as="a"
+            </InternalAnchor>
+            <InternalAnchor
                 href={"#"}
                 title={"Top"}
                 className="top-button button"
-                onClick={() => {
-                        dispatch(
-                            navActions.setCurrentPage("#")
-                        );
-                }}
             >
                 <span className="icon">^</span>
                 <span className="name">Top</span>
-            </ToolbarItem>
-            <ToolbarItem
-                {...toolbar}
-                as="a"
-                href={navTargets.next?.id || "#"}
+            </InternalAnchor>
+            <InternalAnchor
+                href={navTargets.next?.href || "#"}
                 title={
                     navTargets.next ? `Next (${navTargets.next.title})` : "Next"
                 }
                 className="next-button button"
-                disabled={!navTargets.next}
-                onClick={() => {
-                    if (navTargets.next) {
-                        dispatch(
-                            navActions.setCurrentPage(
-                                navTargets.next.id || null
-                            )
-                        );
-                    }
-                }}
             >
                 <span className="name">Next</span>
                 <span className="icon">&gt;</span>
-            </ToolbarItem>
+            </InternalAnchor>
         </div>
     );
 }
