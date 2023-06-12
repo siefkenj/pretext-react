1fefade91ab3889129b42476b371f81eef9df5c2
Jason Siefken <siefkenj@gmail.com>
2023-06-12 15:55:44 -0500

    Explicitly list possible Runestone imports

--- src/state-management/redux-slices/runestone/types.ts
+++ src/state-management/redux-slices/runestone/types.ts
@@ -35,7 +35,36 @@ export interface ACFactory {
     createScratchActivecode: () => {};
     toggleScratchActivecode: () => {};
 }
+
+// Name list taken from https://github.com/RunestoneInteractive/rs/blob/67775377fa134e5124070f99bdb5550aca20f91a/bases/rsptx/interactives/webpack.index.js#L58
+type RunestoneImport =
+    | "activecode"
+    | "ble"
+    | "clickablearea"
+    | "codelens"
+    | "datafile"
+    | "dragndrop"
+    | "fillintheblank"
+    | "groupsub"
+    | "khanex"
+    | "lp_build"
+    | "multiplechoice"
+    | "hparsons"
+    | "parsons"
+    | "poll"
+    | "quizly"
+    | "reveal"
+    | "selectquestion"
+    | "shortanswer"
+    | "showeval"
+    | "simple_sensor"
+    | "spreadsheet"
+    | "tabbedStuff"
+    | "timedAssessment"
+    | "wavedrom"
+    | "webwork"
+    | "youtube";
 export interface RunestoneComponentsGlobal {
     renderOneComponent: (node: HTMLElement) => void;
-    runestone_import: (component_name: string) => Promise<void>;
+    runestone_import: (component_name: RunestoneImport) => Promise<void>;
 }
